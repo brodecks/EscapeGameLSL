b96abbd0e59833de2a9b1d61920d580406aa710d
brodeck <raphael.donikian@outlook.fr>
2026-03-06 16:27:49 +0100

    10 : Metronome orienté pour des étapes (pas opti)

--- metronome.py
+++ metronome.py
@@ -1,32 +1,32 @@
 import time
 from pylsl import StreamInfo, StreamOutlet
-import threading
+from pynput import keyboard
 
 #config
 info = StreamInfo('Metronome', 'Marker', 1, 0, 'int32', 'metronome_001')
 outlet = StreamOutlet(info)
 
-# Variable globale pour le rythme
-bpm_metronome = 60
+print("Appuie sur ESPACE pour envoyer un marqueur LSL !")
+print("Appuie sur ECHAP pour quitter.")
 
 
-def metronome_loop():
-    print(f"Métronome lancé à {bpm_metronome} BPM")
-    while True:
-        outlet.push_sample([bpm_metronome])
-        # Calculer l'attente en fonction du BPM choisi
-        intervalle = 60.0 / bpm_metronome
-        time.sleep(intervalle)
-
-
-#On utilise un thread pour pouvoir changer de bpm avec un input de lutilisateur
-thread = threading.Thread(target=metronome_loop, daemon=True)
-thread.start()
-
-while True:
+#fonction qui traite l'input de la touche espace
+def touche_entree(key):
     try:
-        nouveau_bpm = input("Entrez un nouveau BPM : ")
-        bpm_metronome = int(nouveau_bpm)
-        print(f"Rythme modifié : {bpm_metronome} BPM")
-    except ValueError:
-        print("Veuillez entrer un nombre entier.")
+        if key == keyboard.Key.space:
+            # On envoie le marqueur 99 sur LSL
+            outlet.push_sample([99])
+            print(" [ESPACE] -> Marqueur 99 envoyé à LSL")
+        elif key == keyboard.Key.ctrl_l:
+            outlet.push_sample([80])
+            print(" [ctrl gauche] -> Marqueur 80 envoyé à LSL")
+
+        if key == keyboard.Key.esc:
+            # Arrêter l'écouteur
+            return False
+    except AttributeError:
+        pass
+
+
+with keyboard.Listener(on_press=touche_entree) as listener:
+    listener.join()
